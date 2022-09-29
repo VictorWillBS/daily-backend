0cc31dc040455e153328d9dafecaf9aef8b40e60
Victor Barros <victorwbarros@gmail.com>
2022-09-29 00:27:31 -0300

    :sparkles: Feat: Create error Middleware

---
+++ src/middleware/errorMiddleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import discoverStatusCode from "../utils/discoverStatusCode";
+
+export default async function errorHandle(
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error) {
+    const statusCode = discoverStatusCode(error.code);
+    return res.status(statusCode).send(error.message);
+  }
+
+  res.status(500).send("Internal Server Error");
+}
