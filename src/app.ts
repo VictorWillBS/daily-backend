88cf10507cf6499405ea17f6581cb982b0be6675
Victor Barros <victorwbarros@gmail.com>
2022-09-29 00:33:35 -0300

    :sparkles: Create App

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import express, {json} from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use(json());
 app.use(cors());
 
-
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Servidor rodando na porta " + port);
+});
