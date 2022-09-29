e2d1c43fbff32d2a820ef502b9293f4b0ac63aa3
Victor Barros <victorwbarros@gmail.com>
2022-09-29 02:17:46 -0300

    :sparkles: Feat: Add Crypt Functions

--- src/utils/cryptData.ts
+++ src/utils/cryptData.ts
@@ -1,16 +1,39 @@
 import bcrypt from "bcrypt";
+import Cryptr from "cryptr";
+import dotenv from "dotenv";
+dotenv.config();
+const cryptr = new Cryptr(`${process.env.CRYPTR_SECRET}`);
+function encrypt(data: string) {
+  const dataEncrypt: string = cryptr.encrypt(data);
+  return dataEncrypt;
+}
 
-function encriptByHash(data: string): string {
-  const dataEncript = bcrypt.hashSync(data, 10);
+function decrypt(data: string) {
+  const dataDecrypt: string = cryptr.decrypt(data);
+  return dataDecrypt;
+}
+
+function compareEncrypted(data1: string, data2: string) {
+  const dataDecrypt1: string = cryptr.decrypt(data1);
+  const dataDecrypt2: string = cryptr.decrypt(data2);
+
+  return dataDecrypt1 === dataDecrypt2;
+}
+
+function encriptByHash(data: string) {
+  const dataEncript: string = bcrypt.hashSync(data, 10);
   return dataEncript;
 }
 
-function compareHash(toCompare: string, dataEncript: string): boolean {
-  const isSame = bcrypt.compareSync(toCompare, dataEncript);
+function compareHash(toCompare: string, dataEncript: string) {
+  const isSame: boolean = bcrypt.compareSync(toCompare, dataEncript);
   return isSame;
 }
 
 export default {
   encriptByHash,
   compareHash,
+  encrypt,
+  decrypt,
+  compareEncrypted,
 };
