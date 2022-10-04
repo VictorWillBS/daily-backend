dae9882e6a8f9f0bbce3f3eef24e900007433c50
Victor Barros <coffiekingb@gmail.com>
2022-10-04 01:59:11 -0300

    :sparkles: Feat:Change Generate Date

--- src/utils/generateDate.ts
+++ src/utils/generateDate.ts
@@ -3,6 +3,6 @@ export default function generateDate(toFormat?: Date) {
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  const dateFormated = `${year}/${month}/${day > 10 ? day : `0${day}`}`;
+  const dateFormated = `${year}-${month}-${day > 10 ? day : `0${day}`}`;
   return dateFormated;
 }
