e927ba47b5cc9c092e8649d67d395ed90adeabc2
Victor Barros <coffiekingb@gmail.com>
2022-10-08 23:26:04 -0300

    :heavy_check_mark: Test: Add Questiom Test

--- tests/unit/questionService.test.ts
+++ tests/unit/questionService.test.ts
@@ -2,6 +2,7 @@ import questionService from "../../src/services/questionService";
 import questionRepository from "../../src/repositories/questionRepository";
 import questionFactory from "../factory/questionFactory";
 import commonFactory from "../factory/commonFactory";
+import * as generateDate from "../../src/utils/generateDate";
 
 beforeEach(() => {
   jest.resetAllMocks();
@@ -109,5 +110,80 @@ describe("Create Question Test", () => {
     expect(questionRepository.getAllAble).toBeCalled();
     expect(questionRepository.enableById).toBeCalled();
     expect(questionRepository.insert).not.toHaveBeenCalled();
+  });
+});
+
+describe("Disable Question Test", () => {
+  it("Muist Disable Question", async () => {
+    const question = questionFactory.allowQuestion();
+    const userId = commonFactory.randomNumber();
+    jest.spyOn(questionRepository, "getById").mockResolvedValueOnce(question);
+    jest.spyOn(questionRepository, "disableById").mockResolvedValueOnce(true);
+
+    const result = await questionService.disableQuestion(question.id, userId);
+
+    expect(result).toBe(true);
+    expect(questionRepository.getById).toBeCalled();
+    expect(questionRepository.disableById).toBeCalled();
+  });
+  it("Muist Return Not Found Question Question", async () => {
+    const question = questionFactory.allowQuestion();
+    const userId = commonFactory.randomNumber();
+    jest.spyOn(questionRepository, "getById").mockResolvedValueOnce(null);
+    jest.spyOn(questionRepository, "disableById");
+
+    const result = questionService.disableQuestion(question.id, userId);
+
+    expect(result).rejects.toStrictEqual({
+      code: "Not Found",
+      message: "Questão não encontrada.",
+    });
+    expect(questionRepository.getById).toBeCalled();
+    expect(questionRepository.disableById).not.toHaveBeenCalled();
+  });
+});
+
+describe("Get Question Test", () => {
+  it("Must Get All Questions", async () => {
+    const question = questionFactory.allowQuestion;
+    const userId = commonFactory.randomNumber();
+    const today = "2022-10-10";
+    const resultExpect = [question(), question()];
+    jest.spyOn(generateDate, "default").mockImplementationOnce(() => today);
+    jest
+      .spyOn(questionRepository, "getAllToday")
+      .mockResolvedValueOnce(resultExpect);
+
+    const result = await questionService.getQuestions(userId);
+
+    expect(result).toStrictEqual(resultExpect);
+    expect(generateDate.default).toBeCalled();
+    expect(questionRepository.getAllToday).toBeCalled();
+  });
+});
+
+describe("Get Question By Date", () => {
+  it("Must Get Question by Date", async () => {
+    const question = questionFactory.allowQuestion;
+    const userId = commonFactory.randomNumber();
+    const date = "2022-10-10";
+    const disableQuestions = [
+      { ...question(), answer: [1, 2] },
+      { ...question(), answer: [1, 2] },
+    ];
+    const todayQuestions = [question(), question()];
+    const resultExpect = [...disableQuestions, ...todayQuestions];
+    jest
+      .spyOn(questionRepository, "getByDate")
+      .mockResolvedValueOnce(disableQuestions);
+    jest
+      .spyOn(questionRepository, "getAllToday")
+      .mockResolvedValueOnce(todayQuestions);
+
+    const result = await questionService.getQuestionsByDate(date, userId);
+
+    expect(result).toStrictEqual(resultExpect);
+    expect(questionRepository.getByDate).toBeCalled();
+    expect(questionRepository.getAllToday).toBeCalled();
   });
 });
