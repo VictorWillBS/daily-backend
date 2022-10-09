1d2305596f7030fdcac6a6bc0b27bcd93f433c0c
Victor Barros <coffiekingb@gmail.com>
2022-10-09 00:42:17 -0300

    :heavy_check_mark: Test: Add Random Alphanumeric into common Factory

--- tests/factory/unit/commonFactory.ts
+++ tests/factory/unit/commonFactory.ts
@@ -3,9 +3,13 @@ function randomNumber() {
   return faker.datatype.number();
 }
 function randomString() {
+  return faker.random.alpha(20);
+}
+function randomAlphaNumeric() {
   return faker.random.alphaNumeric(20);
 }
 export default {
   randomNumber,
   randomString,
+  randomAlphaNumeric,
 };
